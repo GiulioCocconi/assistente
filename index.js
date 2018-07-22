9d8aa08ffced6bbf710c2910ed93104dd3c8d184
GiulioCocconi <31406038+GiulioCocconi@users.noreply.github.com>
2018-07-22 18:15:29 +0000

    Add a switch

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 //req
 const Discord = require('discord.js');
+const music = require('discord.js-music-v11'); //musica
+const YoutubeDL = require('youtube-dl'); //musica
+
 
 //Discord Client
 const client = new Discord.Client();
@@ -25,24 +28,21 @@ client.on('ready', () => {
 
 client.on('message', message => {
 
-//dado
-    if (message.content === prefix + ' ' + 'dado') {
+  if (message.toLowerCase().startsWith(PREFIX.toLowerCase())) {			
+    const comando = message.substring(PREFIX.length).split(/[ \n]/)[0].toLowerCase().trim();
+    const suffisso = message.substring(PREFIX.length + command.length).trim();
 
-       var dado = Math.floor(Math.random() * 6) + 1; //valore del dado
-
-       var dadoImage = 'http://ilserverdiminecraft.altervista.org/Server/Bot/Comandi/Dadi/' + dado + '.png'
-       //url immagine dado
-
+    switch (comando) {
+      case 'dado':
+        var dado = Math.floor(Math.random() * 6) + 1; //valore del dado
+        var dadoImage = 'http://ilserverdiminecraft.altervista.org/Server/Bot/Comandi/Dadi/' + dado + '.png'
+        //url immagine dado
        var messaggio = 'Ho tirato il dado ed è uscito questo numero:' 
-
        message.reply(messaggio + ' ' + dado, {file: dadoImage});
     }
 
-//comando
-  if (message.content === prefix + 'comando') {
-
- }
+    }
+  }
 });
-
 //login al bot, da mettere alla fine
 client.login(process.env.TOKEN)
