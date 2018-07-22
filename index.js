4be7e131b41e94193be9beb643db9399187799e0
GiulioCocconi <31406038+GiulioCocconi@users.noreply.github.com>
2018-07-22 16:20:26 +0000

    Added "dado" command

--- index.js
+++ index.js
@@ -11,10 +11,24 @@ function error (message) {
   console.log(message)
 }
 
+//quando il bot si accende
+
 client.on('ready', () => {
   console.log('Il bot è pronto')
   client.user.setGame('gestire il server')
 })
 
+//quando si  manda un messaggio
+
+//dado
+client.on('message', message => {
+
+    if (message.content === prefix + 'dado') {
+
+       message.reply(Math.floor(Math.random() * 6) + 1);
+
+       }
+
+});
 //login al bot, da mettere alla fine
 client.login(process.env.TOKEN)
