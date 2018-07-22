dbe89de55cf67a2627ca47685fddad7abe6cdb7c
GiulioCocconi <31406038+GiulioCocconi@users.noreply.github.com>
2018-07-22 16:46:26 +0000

    Update index.js

--- index.js
+++ index.js
@@ -18,17 +18,49 @@ client.on('ready', () => {
   client.user.setGame('gestire il server')
 })
 
+
+
 //quando si  manda un messaggio
 
-//dado
+
 client.on('message', message => {
-
+//dado
     if (message.content === prefix + 'dado') {
+       var dadoImage //url immagine dado
+       var message = 'Ho tirato il dado ed è uscito"
+       var dado = Math.floor(Math.random() * 6) + 1; //valore del dado
 
-       message.reply(Math.floor(Math.random() * 6) + 1);
-
+       if (dado === 1) {
+          dadoImage = 'http://ilserverdiminecraft.altervista.org/Server/Bot/Comandi/Dadi/1.png'
        }
 
-});
+       if (dado === 2) {
+          dadoImage = 'http://ilserverdiminecraft.altervista.org/Server/Bot/Comandi/Dadi/2.png'
+       }
+
+       if (dado === 3) {
+          dadoImage = 'http://ilserverdiminecraft.altervista.org/Server/Bot/Comandi/Dadi/3.png'
+       }
+
+       if (dado === 4) {
+          dadoImage = 'http://ilserverdiminecraft.altervista.org/Server/Bot/Comandi/Dadi/4.png'
+       }
+
+       if (dado === 5) {
+          dadoImage = 'http://ilserverdiminecraft.altervista.org/Server/Bot/Comandi/Dadi/5.png'
+       }
+
+       if (dado === 6) {
+          dadoImage = 'http://ilserverdiminecraft.altervista.org/Server/Bot/Comandi/Dadi/6.png'
+       }
+
+  
+
+           
+
+       message.reply(dadoImage + message + dado)
+      }
+   }
+);
 //login al bot, da mettere alla fine
 client.login(process.env.TOKEN)
