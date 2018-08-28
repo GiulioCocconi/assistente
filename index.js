5fa17a2258d7431e38e81d22cb563eecde41e3f1
GiulioCocconi <31406038+GiulioCocconi@users.noreply.github.com>
2018-08-28 08:34:49 +0000

    restored Index.js

--- index.js
+++ index.js
@@ -1,120 +1,46 @@
 //req
-
 const Discord = require('discord.js');
 
-
-
-
 //Discord Client
-
 const client = new Discord.Client();
 
-
-
-
 //prefisso
-
 var prefix = "!"
 
-
-
-
 function error (message) {
-
   console.log(message)
-
 }
-
-
-
 
 //quando il bot si accende
 
-
-
-
 client.on('ready', () => {
-
   console.log('Il bot è pronto')
-
   client.user.setActivity('gestire il server')
-
 })
 
 
 
-
-
-
-
-
-
-
 //quando si  manda un messaggio
-
  client.on('message', message => {
-
    if (message.content.startsWith(prefix)) {
-
-     var messaggioArray = message.content.split(' ');
-
-     var comando = messaggioArray[0];
-
-     var arg = messaggioArray.slice(1)
-
+     let messaggioArray = message.content.split(' ');
+     let comando = messaggioArray[0];
+     let arg = messaggioArray.slice(1)
      
-
      //dado
-
      if (comando === prefix + 'dado') {
-       let dado = Math.floor(Math.random() * 6) + 1; //valore del dado
-       let dadoImage = 'http://ilserverdiminecraft.altervista.org/Server/Bot/Comandi/Dadi/' + dado + '.png'  //url immagine dado
-       let messaggio = 'Ho tirato il dado ed è uscito questo numero:' 
+       var dado = Math.floor(Math.random() * 6) + 1; //valore del dado
+       var dadoImage = 'http://ilserverdiminecraft.altervista.org/Server/Bot/Comandi/Dadi/' + dado + '.png'  //url immagine dadovar messaggio = 'Ho tirato il dado ed è uscito questo numero:' 
        message.reply(messaggio + ' ' + dado, {file: dadoImage});
      }
+     //comando
+     if (comando === prefix + 'comando') {
 
-     //wolfram API
-
-     if (comando === prefix +  'wolfram') {
-
-       var search
-
-       var b 
-
-       var a
-
-       for (a=1; messaggioArray.slice(a) = ''; a++){
-
-
-           if (a=1){
-
-              search = messaggioArray.slice(a)
-
-              b = search
-
-           }
-
-
-           else {
-
-              search = b +'+'+ messaggioArray.slice(a)
-
-              b = search
-
-           }
-
-       }
-
-       var wolframLink = 'http://api.wolframalpha.com/v1/simple?i=' + search + '&appid=' + process.env.APPID_WOLFRAM + '&background=282b30&foreground=white'
-       message.reply('Ho cercato quello che mi hai chiesto su Wolfram Alpha: /n', {file: wolframLink});
-
-
-
+    //cosa fare
     }
-
      
-
    }
-
   });
 
+//login al bot, da mettere alla fine
+client.login(process.env.TOKEN)
