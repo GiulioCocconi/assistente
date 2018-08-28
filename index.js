473b5c3985a62aba66822e720a47800e3a681818
GiulioCocconi <31406038+GiulioCocconi@users.noreply.github.com>
2018-08-28 08:58:25 +0000

    Added ciao reaction

--- index.js
+++ index.js
@@ -29,18 +29,36 @@ client.on('ready', () => {
      
      //dado
      if (comando === prefix + 'dado') {
-       var dado = Math.floor(Math.random() * 6) + 1; //valore del dado
-       var dadoImage = 'http://ilserverdiminecraft.altervista.org/Server/Bot/Comandi/Dadi/' + dado + '.png'  //url immagine dadovar messaggio = 'Ho tirato il dado ed è uscito questo numero:' 
+       let dado = Math.floor(Math.random() * 6) + 1; //valore del dado
+       let dadoImage = 'http://ilserverdiminecraft.altervista.org/Server/Bot/Comandi/Dadi/' + dado + '.png'  //url immagine dadovar messaggio = 'Ho tirato il dado ed è uscito questo numero:' 
        message.reply(messaggio + ' ' + dado, {file: dadoImage});
      }
-     //comando
-     if (comando === prefix + 'comando') {
 
-    //cosa fare
+     //parliamo
+     if (comando === prefix + 'parliamo') {
+         //cosa fare
+     }
+
     }
      
    }
-  });
+
+if (message.content.startsWith('Ciao')) {
+   let frase = Math.floor(Math.random() * 3) + 1; //numero casuale tra 1 e 4
+   if (frase === 1) {
+      message.channel.send('Ciao a te,' + ' ' + message.author);
+  }
+  if (frase === 2) {
+     message.channel.send('Ciao boss');
+  }
+  if (frase === 3) {
+     message.channel.send('Buongiorno');
+  }
+  if (frase === 4) {
+     message.channel.send('Ciao');
+  }
+}
+});
 
 //login al bot, da mettere alla fine
 client.login(process.env.TOKEN)
